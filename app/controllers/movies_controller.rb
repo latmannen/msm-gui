713c0bf99bb00194b91e09f6204e8d7ad120a72d
latmannen <163900633+latmannen@users.noreply.github.com>
2024-04-30 16:51:31 +0000

    created movies form

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,4 +14,30 @@ class MoviesController < ApplicationController
 
     render({ :template => "movie_templates/show" })
   end
+
+  def create
+    #params hash: 
+    # {"the_title"=>"1", "the_year"=>"1", "the_duration"=>"2", "the_description"=>"3", "the_image"=>"4", "the_director_id"=>"5"}
+    #retrieve users inputs from params
+    m = Movie.new
+    m.title = params.fetch("title_field")
+    m.year = params.fetch("the_year")
+    m.duration = params.fetch("the_duration")
+    m.description = params.fetch("the_description")
+    m.image = params.fetch("the_image")
+    m.director_id = params.fetch("the_director_id")
+ 
+    m.save
+
+    redirect_to("/movies")
+    #create record in movie table
+    
+    #population columns with user input
+    
+    #save
+
+    #Redirect the user back to /movies URL
+
+
+  end
 end
